a9a77beff442e2448b67d5be56a3bd7727c0312b
Pana <130642227+Panapv02@users.noreply.github.com>
2023-12-05 19:37:23 +0100

    Update mapper.py
    habia un fallito jiji

--- exercizo1.1/mapper.py
+++ exercizo1.1/mapper.py
@@ -10,6 +10,6 @@ import sys
 
 for line in sys.stdin:
     data = line.strip().split("\t")
-    if  len(line) < 6: continue;
+    if  len(data) != 6: continue;
     date, time, store, item, cost, payment = data
     print(store+"\t"+cost)
